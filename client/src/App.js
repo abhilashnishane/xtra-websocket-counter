2ece8e0bf55fb73ab008dae7aee36fc0a8c9fc8e
Abhilash Nishane <abhilash.nishane6@gmail.com>
2020-11-25 22:10:09 +0530

    Home component added

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,16 @@ import './App.css';
 import Participant from './Pages/Participant/Participant';
 import Host from './Pages/Host/Host';
 
+function Home() {
+  return (
+    <h3>Go to /host and /participant</h3>
+  )
+}
+
 function App() {
   return (
     <Router>
+      <Route exact path="/" component={Home} />
       <Route exact path="/host" component={Host} />
       <Route exact path="/participant" component={Participant} />
     </Router>
