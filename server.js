9334240f0af31adfa76bc458d168be5b775222b9
Abhilash Nishane <abhilash.nishane6@gmail.com>
2020-11-25 22:30:45 +0530

    attempt 2: fallback for router added in serverjs

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ io.on('connection', (socket) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/client/build/index.html'));
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 server.on('error', (err) => {
